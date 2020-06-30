246e4b1e3fe9e931f01c5ff763c0fd2cec9247df
MeryJanaH <65432555+MeryJanaH@users.noreply.github.com>
2020-06-30 19:54:04 +0200

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,44 @@
+<?php
+
+function login($email,$mdp){
+    require 'LBD.php';
+    $req = $bdd->prepare("SELECT * FROM Admin WHERE Email=:email AND Password=:mdp ");
+    $req->bindParam(':email', $email);
+    $req->bindParam(':mdp', $mdp);
+    if( $req->execute()){
+      $res = $req->fetch();
+      if($res != NULL)
+      {
+      $array = array(
+        "admin" => true,
+        "commercial" => false,
+        "res" => $res
+        );
+        return $array;
+      }
+      else
+      {
+          $req = $bdd->prepare("SELECT * FROM Commerciaux WHERE Email=:email AND Password=:mdp ");
+          $req->bindParam(':email', $email);
+          $req->bindParam(':mdp', $mdp);
+          if( $req->execute())
+          {
+            $res = $req->fetch();
+            if($res != NULL)
+            {
+              $array = array(
+              "admin" => false,
+              "commercial" => true,
+              "res" => $res
+              );
+              return $array;
+            }
+            else {
+              return "Utilisateur Non Enregistré";
+            }
+          }
+      }
+     }
+     return "ERROR_Syntaxe";
+}
+?>
