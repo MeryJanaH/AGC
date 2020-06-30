f8ea69f47b89df636bae91c5b3acf686f4cad6bc
MeryJanaH <65432555+MeryJanaH@users.noreply.github.com>
2020-06-30 20:13:51 +0200

    sess_log

--- traite.php
+++ traite.php
@@ -8,11 +8,13 @@ session_start();
           if (!isset($_POST['email_2']) && !isset($_SESSION['password_2'])) {
               session_destroy();
               unset($_SESSION);
+              header('Location: login.php');
             }
             else {
               $res=login($_POST['email_2'],$_POST['password_2']);
 
                 if ($res=="Utilisateur Non Enregistré" or $res =="ERROR_Syntaxe") {
+                    $_SESSION['login']=false;
                     header('Location: login.php');
                 }else
                     header('Location: index.php');
