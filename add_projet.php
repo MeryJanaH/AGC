11301e219d50ab2782ef9113033b61d3bd3630c3
Yassine <you@example.com>
2020-07-07 17:58:39 +0100

    Projets

--- add_projet.php
+++ add_projet.php
@@ -7,7 +7,7 @@ if(isset($_POST["proj_name"]))
     {
         for ($a = 0; $a < count($_POST["proj_name"]); $a++)
         {
-            $req = $bdd->prepare("INSERT INTO projets (ProjetName, type_p,Etages,Surface,Prix) VALUES ('" . $_POST["proj_name"][$a] . "','" . $_POST["proj_type"][$a]."','" . $_POST["proj_etage"][$a]."','" . $_POST["proj_surface"][$a]."','" . $_POST["proj_prix"][$a]."')");
+            $req = $bdd->prepare("INSERT INTO Projets (ProjetName, type_p,Etages,Surface,Prix) VALUES ('" . $_POST["proj_name"][$a] . "','" . $_POST["proj_type"][$a]."','" . $_POST["proj_etage"][$a]."','" . $_POST["proj_surface"][$a]."','" . $_POST["proj_prix"][$a]."')");
             $req->execute();
         }
         header('Location: ui-projets.php');
