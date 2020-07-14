6f9f238adfcf377059d067ce21c496fd567c1f9a
Yassine <you@example.com>
2020-07-14 21:53:52 +0100

    Chart

--- stats.php
+++ stats.php
@@ -267,18 +267,31 @@
 
 
                     <div class="container-fluid page__container">
-                        <div class="card">
-                            <div class="card-header card-header-large bg-white">
-                                <h4 class="card-header__title">Bar</h4>
-                            </div>
-                            <div class="card-body">
-                                <p>A bar chart provides a way of showing data values represented as vertical bars. It is sometimes used to show trend data, and the comparison of multiple data sets side by side.</p>
-
-                                <div class="chart">
-                                    <canvas id="ordersChart" class="chart-canvas"></canvas>
-                                </div>
-                            </div>
-                        </div>
+                      <div class="card">
+                          <div class="card-header card-header-large bg-white d-flex align-items-center">
+                              <h4 class="card-header__title flex">Switch Toggle</h4>
+                              <div class="d-flex align-items-center">
+                                  <label for="chart-switch-toggle" class="mb-0">Show affiliate:</label>
+                                  <div class="custom-control custom-checkbox-toggle ml-2">
+                                      <input checked="" aria-checked="true" type="checkbox" id="chart-switch-toggle" class="custom-control-input" role="switch" data-toggle="chart" data-target="#ordersChartSwitch" data-add='{"data":{"datasets":[
+                                      {"data":[15,10,20,12,7,0,8,16,18,16,10,22],"backgroundColor":"#b2e599","label":"Wafae1"},
+                                      {"data":[5,10,21,12,7,10,8,16,18,16,10,22],"backgroundColor":"#b2e549","label":"Wafae2"},
+                                      {"data":[5,10,21,12,7,10,8,16,18,16,10,22],"backgroundColor":"#b205a9","label":"Walili1"},
+                                      {"data":[5,10,21,12,7,10,8,16,18,16,10,22],"backgroundColor":"#b2e59b","label":"Walili2"},
+                                      {"data":[5,10,21,12,7,10,8,16,18,16,10,22],"backgroundColor":"#b0e5c9","label":"Hanae1"},
+                                      {"data":[5,10,21,12,7,10,8,16,18,16,10,22],"backgroundColor":"#b2e509","label":"Hanae2"},
+                                      {"data":[5,10,21,12,7,10,8,16,18,16,10,22],"backgroundColor":"#a2e5d9","label":"Hanae3"}]}}'>
+                                      <label class="custom-control-label" for="chart-switch-toggle"><span class="sr-only">Show affiliate</span></label>
+                                  </div>
+                              </div>
+                          </div>
+                          <div class="card-body">
+                              <p>Easily toggle an additional set of data with a simple interface based on the <code>data</code> attributes.</p>
+                              <div class="chart">
+                                  <canvas id="ordersChartSwitch" class="chart-canvas"></canvas>
+                              </div>
+                          </div>
+                      </div>
 
                     </div>
 
