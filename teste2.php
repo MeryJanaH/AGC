cda3e4a2ce6b6ffaa70a08735f88eca99b73d62e
MeryJanaH <65432555+MeryJanaH@users.noreply.github.com>
2020-07-18 22:21:44 +0200

    amélio

--- teste2.php
+++ teste2.php
@@ -1,6 +1,12 @@
 <?php
 require 'functions.php';
-$_SESSION['current_page']="projets";
+
+$_SESSION['current_page']="Graphe";
+
+if(isset($_SESSION['login']) and $_SESSION['login']=="false" or !isset($_SESSION['login']))
+{
+      header('Location: login.php');
+}
 
 ?>
 
@@ -20,19 +26,9 @@ $_SESSION['current_page']="projets";
 
         <!-- App CSS -->
         <link type="text/css" href="assets/css/app.css" rel="stylesheet">
-        <link type="text/css" href="assets/css/app.rtl.css" rel="stylesheet">
 
         <!-- Material Design Icons -->
         <link type="text/css" href="assets/css/vendor-material-icons.css" rel="stylesheet">
-        <link type="text/css" href="assets/css/vendor-material-icons.rtl.css" rel="stylesheet">
-
-        <!-- Font Awesome FREE Icons -->
-        <link type="text/css" href="assets/css/vendor-fontawesome-free.css" rel="stylesheet">
-        <link type="text/css" href="assets/css/vendor-fontawesome-free.rtl.css" rel="stylesheet">
-
-        <!-- ion Range Slider -->
-        <link type="text/css" href="assets/css/vendor-ion-rangeslider.css" rel="stylesheet">
-        <link type="text/css" href="assets/css/vendor-ion-rangeslider.rtl.css" rel="stylesheet">
 
 
         <script src="//cdnjs.cloudflare.com/ajax/libs/list.js/1.5.0/list.min.js"></script>
@@ -49,7 +45,7 @@ $_SESSION['current_page']="projets";
                 <div class="mdk-header-layout js-mdk-header-layout" data-has-scrolling-region>
 
               <?php include 'haute_bar.php';
-  function tables($id){ ?>
+                   function tables($id){ ?>
                     <div class="card card-form">
                         <div class="row no-gutters">
                             <div class="col-lg-12 card-form__body border-left">
@@ -65,7 +61,7 @@ $_SESSION['current_page']="projets";
                                                 <th class="sort" data-sort="Connaissance">Connaissance</th>
                                                 <th class="sort" data-sort="Annonce">Annonce</th>
                                                 <th class="sort" data-sort="Passage">De passage</th>
-                                               <tr> <th class="Total">Total</th> </tr>
+                                                <tr> <th class="Total">Total</th> </tr>
                                               </tr>
                                             </thead>
                                             <tbody class="list">
@@ -212,7 +208,6 @@ $_SESSION['current_page']="projets";
     <script src="assets/vendor/jquery.min.js"></script>
 
     <!-- Bootstrap -->
-    <script src="assets/vendor/popper.min.js"></script>
     <script src="assets/vendor/bootstrap.min.js"></script>
 
     <!-- Simplebar -->
@@ -224,15 +219,8 @@ $_SESSION['current_page']="projets";
     <!-- MDK -->
     <script src="assets/vendor/material-design-kit.js"></script>
 
-    <!-- Range Slider -->
-    <script src="assets/vendor/ion.rangeSlider.min.js"></script>
-    <script src="assets/js/ion-rangeslider.js"></script>
 
     <!-- App -->
-    <script src="assets/js/toggle-check-all.js"></script>
-    <script src="assets/js/check-selected-row.js"></script>
-    <script src="assets/js/dropdown.js"></script>
-    <script src="assets/js/sidebar-mini.js"></script>
     <script src="assets/js/app.js"></script>
 
     <!-- App Settings (safe to remove) -->
