f1033dae516d6a9063c6428c80cfbe46669e99ed
Yassine <you@example.com>
2020-07-21 14:21:04 +0100

    espace

--- Statistiques.php
+++ Statistiques.php
@@ -223,7 +223,7 @@ $d2 = $req2->fetch();
                           $dn5 = $req -> execute();
                           for ($x = 1; $x <= $d2['count_pj']; $x++) {
                             $dn4 = $req3->fetch();
-                            $dn=$req->fetch();
+                            $dn = $req->fetch();
                             if($x != $d2['count_pj']){ ?>
                             {type: 'bar', label:'<?php echo $dn4['ProjetName']; ?>', data:[<?php echo $dn[1]. "," .$dn[2]. "," .$dn[3]. "," .$dn[4]. "," .$dn[5].
                                                                                       "," .$dn[6]. "," .$dn[7]. "," .$dn[8]. "," .$dn[9]. "," .$dn[10]. "," .$dn[11].
