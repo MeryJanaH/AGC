af58f600a80c6b245c28319d9078e98b00b408c8
MeryJanaH <65432555+MeryJanaH@users.noreply.github.com>
2020-07-21 18:02:50 +0200

    stat

--- Statistiques.php
+++ Statistiques.php
@@ -117,46 +117,11 @@ if(isset($_SESSION['login']) and $_SESSION['login']=="false" or !isset($_SESSION
              function annee($year)
              {
                require 'LBD.php';
-               $sql="SELECT ProjetName,IFNULL(Janv,0) AS Janv,IFNULL(Fév,0) AS Fév,IFNULL(Mars,0) AS Mars,IFNULL(Avril,0) AS Avril,IFNULL(Mai,0) AS Mai,IFNULL(Juin,0) AS Juin,IFNULL(Juil,0) AS Juil,IFNULL(Août,0) AS Août,
-                                       IFNULL(Sep,0) AS Sep,IFNULL(Oct,0) AS Oct,
-                                       IFNULL(Nov,0) AS Nov,IFNULL(Déc,0) AS Déc
-               FROM (SELECT Code_pj, ProjetName FROM Projets) t1
-                                     LEFT JOIN
-                    (SELECT Code_pj, Visite,COUNT(Visite)as Janv , date_tdebut FROM Calendrier WHERE YEAR(date_tdebut) = $year AND MONTH(date_tdebut)='01' AND DAY(date_tdebut) >= '01' AND DAY(date_tdebut) <= '31' AND Visite = 'Chantier' GROUP BY Code_pj) t2
-                                     ON t1.Code_pj = t2.Code_pj
-                                     LEFT JOIN
-                     (SELECT Code_pj, Visite,COUNT(Visite)as Fév , date_tdebut FROM Calendrier WHERE YEAR(date_tdebut) = $year AND MONTH(date_tdebut)='02' AND DAY(date_tdebut) >= '01' AND DAY(date_tdebut) <= '31' AND Visite = 'Chantier' GROUP BY Code_pj) t3
-                                     ON t1.Code_pj = t3.Code_pj
-                                     LEFT JOIN
-                     (SELECT Code_pj, Visite,COUNT(Visite)as Mars , date_tdebut FROM Calendrier WHERE YEAR(date_tdebut) = $year AND MONTH(date_tdebut)='03' AND DAY(date_tdebut) >= '01' AND DAY(date_tdebut) <= '31' AND Visite = 'Chantier' GROUP BY Code_pj) t4
-                                     ON t1.Code_pj = t4.Code_pj
-                                     LEFT JOIN
-                     (SELECT Code_pj, Visite,COUNT(Visite)as Avril , date_tdebut FROM Calendrier WHERE YEAR(date_tdebut) = $year AND MONTH(date_tdebut)='04' AND DAY(date_tdebut) >= '01' AND DAY(date_tdebut) <= '31' AND Visite = 'Chantier' GROUP BY Code_pj) t5
-                                     ON t1.Code_pj = t5.Code_pj
-                                     LEFT JOIN
-                     (SELECT Code_pj, Visite,COUNT(Visite)as Mai , date_tdebut FROM Calendrier WHERE YEAR(date_tdebut) = $year AND MONTH(date_tdebut)='05' AND DAY(date_tdebut) >= '01' AND DAY(date_tdebut) <= '31' AND Visite = 'Chantier' GROUP BY Code_pj) t6
-                                     ON t1.Code_pj = t6.Code_pj
-                                     LEFT JOIN
-                     (SELECT Code_pj, Visite,COUNT(Visite)as Juin , date_tdebut FROM Calendrier WHERE YEAR(date_tdebut) = $year AND MONTH(date_tdebut)='06' AND DAY(date_tdebut) >= '01' AND DAY(date_tdebut) <= '31' AND Visite = 'Chantier' GROUP BY Code_pj) t7
-                                     ON t1.Code_pj = t7.Code_pj
-                                     LEFT JOIN
-                     (SELECT Code_pj, Visite,COUNT(Visite)as Juil , date_tdebut FROM Calendrier WHERE YEAR(date_tdebut) = $year AND MONTH(date_tdebut)='07' AND DAY(date_tdebut) >= '01' AND DAY(date_tdebut) <= '31' AND Visite = 'Chantier' GROUP BY Code_pj) t8
-                                     ON t1.Code_pj = t8.Code_pj
-                                     LEFT JOIN
-                     (SELECT Code_pj, Visite,COUNT(Visite)as Août , date_tdebut FROM Calendrier WHERE YEAR(date_tdebut) = $year AND MONTH(date_tdebut)='08' AND DAY(date_tdebut) >= '01' AND DAY(date_tdebut) <= '31' AND Visite = 'Chantier' GROUP BY Code_pj) t9
-                                     ON t1.Code_pj = t9.Code_pj
-                                     LEFT JOIN
-                     (SELECT Code_pj, Visite,COUNT(Visite)as Sep , date_tdebut FROM Calendrier WHERE YEAR(date_tdebut) = $year AND MONTH(date_tdebut)='09' AND DAY(date_tdebut) >= '01' AND DAY(date_tdebut) <= '31' AND Visite = 'Chantier' GROUP BY Code_pj) t10
-                                     ON t1.Code_pj = t10.Code_pj
-                                     LEFT JOIN
-                     (SELECT Code_pj, Visite,COUNT(Visite)as Oct , date_tdebut FROM Calendrier WHERE YEAR(date_tdebut) = $year AND MONTH(date_tdebut)='10' AND DAY(date_tdebut) >= '01' AND DAY(date_tdebut) <= '31' AND Visite = 'Chantier' GROUP BY Code_pj) t11
-                                     ON t1.Code_pj = t11.Code_pj
-                                     LEFT JOIN
-                     (SELECT Code_pj, Visite,COUNT(Visite)as Nov , date_tdebut FROM Calendrier WHERE YEAR(date_tdebut) = $year AND MONTH(date_tdebut)='11' AND DAY(date_tdebut) >= '01' AND DAY(date_tdebut) <= '31' AND Visite = 'Chantier' GROUP BY Code_pj) t12
-                                     ON t1.Code_pj = t12.Code_pj
-                                     LEFT JOIN
-                     (SELECT Code_pj, Visite,COUNT(Visite)as Déc , date_tdebut FROM Calendrier WHERE YEAR(date_tdebut) = $year AND MONTH(date_tdebut)='12' AND DAY(date_tdebut) >= '01' AND DAY(date_tdebut) <= '31' AND Visite = 'Chantier' GROUP BY Code_pj) t13
-                                     ON t1.Code_pj = t13.Code_pj";
+               $sql="SELECT ProjetName,IFNULL(Janv,0) AS Janv
+                FROM (SELECT Code_pj, ProjetName FROM Projets) t1
+                                      LEFT JOIN
+                     (SELECT Code_pj, Visite,COUNT(Visite)as Janv , date_tdebut FROM Calendrier WHERE YEAR(date_tdebut) = '2020' AND MONTH(date_tdebut)='07' AND DAY(date_tdebut) >= '01' AND DAY(date_tdebut) <= '31' AND Visite = 'Chantier' GROUP BY Code_pj) t2
+                                      ON t1.Code_pj = t2.Code_pj";
 
                $req=$bdd->prepare($sql);
                return $req;
@@ -206,7 +171,7 @@ if(isset($_SESSION['login']) and $_SESSION['login']=="false" or !isset($_SESSION
  if(isset($_GET['year'])){
       $req=annee($_GET['year']);
  } else {
-      //$req=annee(date("Y"));
+      $req=annee(date("Y"));
 
 }
 
@@ -270,58 +235,16 @@ $d2 = $req2->fetch();
                           $colors = array("#00008B", "#D2691E", "#b2e59b", "##FF7F50", "#b2e509", "#a2e5d9","#b2e599","#483D8B","#FF1493","#8B0000","#008000","#66CDAA");
                           $req3 = $bdd->prepare("SELECT ProjetName FROM `Projets`");
                           $dn3 = $req3 -> execute();
-                          $req=$bdd->prepare("SELECT ProjetName,IFNULL(Janv,0) AS Janv,IFNULL(Fév,0) AS Fév,IFNULL(Mars,0) AS Mars,IFNULL(Avril,0) AS Avril,IFNULL(Mai,0) AS Mai,IFNULL(Juin,0) AS Juin,IFNULL(Juil,0) AS Juil,IFNULL(Août,0) AS Août,
-                                                  IFNULL(Sep,0) AS Sep,IFNULL(Oct,0) AS Oct,
-                                                  IFNULL(Nov,0) AS Nov,IFNULL(Déc,0) AS Déc
-                          FROM (SELECT Code_pj, ProjetName FROM Projets) t1
-                                                LEFT JOIN
-                               (SELECT Code_pj, Visite,COUNT(Visite)as Janv , date_tdebut FROM Calendrier WHERE YEAR(date_tdebut) = $year AND MONTH(date_tdebut)='01' AND DAY(date_tdebut) >= '01' AND DAY(date_tdebut) <= '31' AND Visite = 'Chantier' GROUP BY Code_pj) t2
-                                                ON t1.Code_pj = t2.Code_pj
-                                                LEFT JOIN
-                                (SELECT Code_pj, Visite,COUNT(Visite)as Fév , date_tdebut FROM Calendrier WHERE YEAR(date_tdebut) = $year AND MONTH(date_tdebut)='02' AND DAY(date_tdebut) >= '01' AND DAY(date_tdebut) <= '31' AND Visite = 'Chantier' GROUP BY Code_pj) t3
-                                                ON t1.Code_pj = t3.Code_pj
-                                                LEFT JOIN
-                                (SELECT Code_pj, Visite,COUNT(Visite)as Mars , date_tdebut FROM Calendrier WHERE YEAR(date_tdebut) = $year AND MONTH(date_tdebut)='03' AND DAY(date_tdebut) >= '01' AND DAY(date_tdebut) <= '31' AND Visite = 'Chantier' GROUP BY Code_pj) t4
-                                                ON t1.Code_pj = t4.Code_pj
-                                                LEFT JOIN
-                                (SELECT Code_pj, Visite,COUNT(Visite)as Avril , date_tdebut FROM Calendrier WHERE YEAR(date_tdebut) = $year AND MONTH(date_tdebut)='04' AND DAY(date_tdebut) >= '01' AND DAY(date_tdebut) <= '31' AND Visite = 'Chantier' GROUP BY Code_pj) t5
-                                                ON t1.Code_pj = t5.Code_pj
-                                                LEFT JOIN
-                                (SELECT Code_pj, Visite,COUNT(Visite)as Mai , date_tdebut FROM Calendrier WHERE YEAR(date_tdebut) = $year AND MONTH(date_tdebut)='05' AND DAY(date_tdebut) >= '01' AND DAY(date_tdebut) <= '31' AND Visite = 'Chantier' GROUP BY Code_pj) t6
-                                                ON t1.Code_pj = t6.Code_pj
-                                                LEFT JOIN
-                                (SELECT Code_pj, Visite,COUNT(Visite)as Juin , date_tdebut FROM Calendrier WHERE YEAR(date_tdebut) = $year AND MONTH(date_tdebut)='06' AND DAY(date_tdebut) >= '01' AND DAY(date_tdebut) <= '31' AND Visite = 'Chantier' GROUP BY Code_pj) t7
-                                                ON t1.Code_pj = t7.Code_pj
-                                                LEFT JOIN
-                                (SELECT Code_pj, Visite,COUNT(Visite)as Juil , date_tdebut FROM Calendrier WHERE YEAR(date_tdebut) = $year AND MONTH(date_tdebut)='07' AND DAY(date_tdebut) >= '01' AND DAY(date_tdebut) <= '31' AND Visite = 'Chantier' GROUP BY Code_pj) t8
-                                                ON t1.Code_pj = t8.Code_pj
-                                                LEFT JOIN
-                                (SELECT Code_pj, Visite,COUNT(Visite)as Août , date_tdebut FROM Calendrier WHERE YEAR(date_tdebut) = $year AND MONTH(date_tdebut)='08' AND DAY(date_tdebut) >= '01' AND DAY(date_tdebut) <= '31' AND Visite = 'Chantier' GROUP BY Code_pj) t9
-                                                ON t1.Code_pj = t9.Code_pj
-                                                LEFT JOIN
-                                (SELECT Code_pj, Visite,COUNT(Visite)as Sep , date_tdebut FROM Calendrier WHERE YEAR(date_tdebut) = $year AND MONTH(date_tdebut)='09' AND DAY(date_tdebut) >= '01' AND DAY(date_tdebut) <= '31' AND Visite = 'Chantier' GROUP BY Code_pj) t10
-                                                ON t1.Code_pj = t10.Code_pj
-                                                LEFT JOIN
-                                (SELECT Code_pj, Visite,COUNT(Visite)as Oct , date_tdebut FROM Calendrier WHERE YEAR(date_tdebut) = $year AND MONTH(date_tdebut)='10' AND DAY(date_tdebut) >= '01' AND DAY(date_tdebut) <= '31' AND Visite = 'Chantier' GROUP BY Code_pj) t11
-                                                ON t1.Code_pj = t11.Code_pj
-                                                LEFT JOIN
-                                (SELECT Code_pj, Visite,COUNT(Visite)as Nov , date_tdebut FROM Calendrier WHERE YEAR(date_tdebut) = $year AND MONTH(date_tdebut)='11' AND DAY(date_tdebut) >= '01' AND DAY(date_tdebut) <= '31' AND Visite = 'Chantier' GROUP BY Code_pj) t12
-                                                ON t1.Code_pj = t12.Code_pj
-                                                LEFT JOIN
-                                (SELECT Code_pj, Visite,COUNT(Visite)as Déc , date_tdebut FROM Calendrier WHERE YEAR(date_tdebut) = $year AND MONTH(date_tdebut)='12' AND DAY(date_tdebut) >= '01' AND DAY(date_tdebut) <= '31' AND Visite = 'Chantier' GROUP BY Code_pj) t13
-                                                ON t1.Code_pj = t13.Code_pj");
                           $dn5 = $req->execute();
+                          $dn = $req->fetch();
                           for ($x = 1; $x <= $d2['count_pj']; $x++) {
-                            $dn = $req->fetch();
                             if($x != $d2['count_pj']){ ?>
-                            {type: 'bar', label:'<?php echo $dn[0]; ?>', data:[<?php echo $dn[1]. "," .$dn[2]. "," .$dn[3]. "," .$dn[4]. "," .$dn[5].
-                                                                                      "," .$dn[6]. "," .$dn[7]. "," .$dn[8]. "," .$dn[9]. "," .$dn[10]. "," .$dn[11].
-                                                                                     "," .$dn[12] ; ?>], backgroundColor:'<?php echo $colors[$n++]; ?>',borderColor: 'white',
+                            {type: 'bar', label:'<?php echo $dn['ProjetName']; ?>', data:[<?php echo $dn[1]. "," .$dn[1]. "," .$dn[1]. "," .$dn[1]. "," .$dn[1];
+                                                                                       ?>], backgroundColor:'<?php echo $colors[$n++]; ?>',borderColor: 'white',
                     				borderWidth: 1}, <?php
                             }else { ?>
-                              {type: 'bar', label:'<?php echo $dn[0]; ?>', data:[<?php echo $dn[1]. "," .$dn[2]. "," .$dn[3]. "," .$dn[4]. "," .$dn[5].
-                                                                                        "," .$dn[6]. "," .$dn[7]. "," .$dn[8]. "," .$dn[9]. "," .$dn[10]. "," .$dn[11].
-                                                                                       "," .$dn[12] ; ?>], backgroundColor:'<?php echo $colors[$n++]; ?>',borderColor: 'white',
+                              {type: 'bar', label:'<?php echo $dn[0]; ?>', data:[<?php echo $dn[1]. "," .$dn[1]. "," .$dn[1]. "," .$dn[1]. "," .$dn[1];
+                                                                                         ?>], backgroundColor:'<?php echo $colors[$n++]; ?>',borderColor: 'white',
                       				borderWidth: 1} <?php
                             }
                           } ?>
