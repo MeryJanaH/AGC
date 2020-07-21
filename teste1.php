80c3cd96d6ad3edce67eaff17be0d58b5d933969
MeryJanaH <65432555+MeryJanaH@users.noreply.github.com>
2020-07-21 10:04:22 +0200

    unused

--- teste1.php
+++
@@ -1,6 +0,0 @@
-{
-    enableTime: true,
-    noCalendar: true,
-    dateFormat: "H:i",
-    time_24hr: true
-}
--- ttt.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'LBD.php';
-
-$colors = array("#b2e599", "#b205a9", "#b2e59b", "#b0e5c9", "#b2e509", "#a2e5d9","#b2e599");
-echo $colors[1];
-?>
